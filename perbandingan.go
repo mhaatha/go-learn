4359fa95f2ce2f3262eb1922a9e5f20703e5c142
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-27 22:52:57 +0800

    go compare

---
+++ perbandingan.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "Joko"
+	var name2 = "Joko"
+
+	var result bool = name1 == name2
+
+	fmt.Println(result)
+
+	var a = "a"
+	var b = "b"
+
+	fmt.Println(a > b)
+}
