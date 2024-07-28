03d5498263f0e514a2d353480589d1c2487ee84f
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-28 13:33:45 +0800

    level 2

---
+++ go-fundamental/intro/2-bug/code.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	messagesFromDoris := []string{
+		"You doing anything later??",
+		"Did you get my last message?",
+		"Don't leave me hanging...",
+		"Please respond I'm lonely!",
+	}
+	numMessages := float64(len(messagesFromDoris))
+	costPerMessage := .02
+
+	// don't touch above this line
+
+	totalCost := costPerMessage + numMessages
+
+	// don't touch above this line
+
+	fmt.Printf("Doris spent %.2f on text message today\n", totalCost)
+}
