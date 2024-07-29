4f83c04b5722431d087b317a5d90bb30df1ceaab
Hafidz Athaya <mhafidza528@gmail.com>
2024-07-29 11:19:09 +0800

    level 4

---
+++ go-fundamental/02-variables/04-same_line_declarations/complete.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare here
+	averageOpenRate, displayMessage := .23, "is the average open rate of your message"
+
+	fmt.Println(averageOpenRate, displayMessage)
+}
