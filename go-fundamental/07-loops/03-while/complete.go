49729873bc5c1811d9959bcde28c6ed37d29ece0
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-01 11:42:19 +0800

    level 3

--- go-fundamental/07-loops/03-while/complete.go
+++ go-fundamental/07-loops/03-while/complete.go
@@ -5,7 +5,7 @@ import "fmt"
 func getMaxMessagesToSend(costMultiplier float64, maxCostInPennies int) int {
 	actualCostInPennies := 1.0
 	maxMessagesToSend := 0
-	for actualCostInPennies < float64(maxCostInPennies) {
+	for actualCostInPennies <= float64(maxCostInPennies) {
 		maxMessagesToSend++
 		actualCostInPennies *= costMultiplier
 	}
