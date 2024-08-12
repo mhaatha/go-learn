73ae3f7a49bd06aad4a2baeb5b463b1b2c4402f9
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-12 15:23:53 +0800

    Go regexp standard library

---
+++ go-standard-library/regexp.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	regex := regexp.MustCompile(`e([a-z])o`)
+
+	fmt.Println(regex.MatchString("eko"))
+	fmt.Println(regex.MatchString("edo"))
+	fmt.Println(regex.MatchString("eXo"))
+
+	fmt.Println(regex.FindAllString("ekoegoeboexoeroetoeoo", 10))
+}
