677d6fdbb6d2435c5c019aee94539d3546ee5754
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-12 12:20:49 +0800

    Go os standard library

---
+++ go-standard-library/os.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println(hostname)
+}
