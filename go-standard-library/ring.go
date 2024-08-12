7436efb5eec3ebaf0f144a9d04033a3cf8824062
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-12 14:10:25 +0800

    Go container/ring standard library

---
+++ go-standard-library/ring.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"container/ring"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	data := ring.New(5)
+
+	for i := 0; i < data.Len(); i++ {
+		data.Value = "Value " + strconv.Itoa(i)
+		data = data.Next()
+	}
+
+	data.Do(func(value any) {
+		fmt.Println(value)
+	})
+}
