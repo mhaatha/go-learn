cc898e140cb0c3a0c5edfb5b80ec296b26a019c0
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-12 16:21:14 +0800

    Go csv reader standard library

---
+++ go-standard-library/csv_reader.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	csvString := "eko,kurniawan,khannedy\n" +
+		"budi,pratama,anugrah\n" +
+		"joko,morro,diah"
+
+	reader := csv.NewReader(strings.NewReader(csvString))
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(record)
+	}
+}
