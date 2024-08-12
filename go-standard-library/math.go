1b69fb0828ba13eecaf71a89dd01656e4cd7a88e
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-12 13:53:51 +0800

    Go math standard library

---
+++ go-standard-library/math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.40))
+	fmt.Println(math.Floor(1.40))
+	fmt.Println(math.Round(1.40))
+	fmt.Println(math.Max(1.40, 12))
+	fmt.Println(math.Min(1.40, 12))
+}
