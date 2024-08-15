f0ca685a5e8186559f62c4ade323b445e155e905
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-15 19:52:11 +0800

    Change error handling from panic to Fatal and Error

--- go-unit-test/helper/hello_world_test.go
+++ go-unit-test/helper/hello_world_test.go
@@ -1,19 +1,28 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldEko(t *testing.T) {
 	result := HelloWorld("Eko")
 
 	if result != "Hello Eko" {
-		panic("Result is not 'Hello Eko'")
+		// panic("Result is not 'Hello Eko'") // Bad Error Handling
+		t.Error("Result must be 'Hello Eko'")
 	}
+
+	fmt.Println("TestHelloWorldEko Done")
 }
 
 func TestHelloWorldKhannedy(t *testing.T) {
 	result := HelloWorld("Khannedy")
 
 	if result != "Hello Khannedy" {
-		panic("Result is not 'Hello Khannedy'")
+		// panic("Result is not 'Hello Khannedy'") // Bad Error Handling
+		t.Fatal("Result must be 'Hello Khannedy'")
 	}
+
+	fmt.Println("TestHelloWorldKhannedy Done")
 }
