b2b3e34a14874ee58cbb16133e3e072b4eb0964b
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-15 20:28:51 +0800

    Using require and assert from package testify

--- go-unit-test/helper/hello_world_test.go
+++ go-unit-test/helper/hello_world_test.go
@@ -3,7 +3,26 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Eko")
+
+	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+
+	fmt.Println("This line will not executed if error")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Eko")
+
+	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+
+	fmt.Println("This line still executed even the test is error")
+}
 
 func TestHelloWorldEko(t *testing.T) {
 	result := HelloWorld("Eko")
