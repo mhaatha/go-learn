9fb6b926cd51c7448da9e0401ef4d5e390c44777
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-18 16:39:51 +0800

    Select channel

--- go-goroutines/channel_test.go
+++ go-goroutines/channel_test.go
@@ -6,6 +6,32 @@ import (
 	"time"
 )
 
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2:", data)
+			counter++
+		}
+		
+		if counter == 2 {
+			break
+		}
+	}
+}
+
 func TestRangeChannel(t *testing.T) {
 	channel := make(chan string, 10)
 
