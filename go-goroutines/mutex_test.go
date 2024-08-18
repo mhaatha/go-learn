ba39fda6ff9f81579a1f7613e00bf968e1aaf5b1
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-18 17:39:26 +0800

    Read write mutex

--- go-goroutines/mutex_test.go
+++ go-goroutines/mutex_test.go
@@ -7,6 +7,40 @@ import (
 	"time"
 )
 
+type BankAccount struct {
+	RWMutex sync.RWMutex
+	Balance int
+}
+
+func (b *BankAccount) AddBalance(amount int) {
+	b.RWMutex.Lock()
+	defer b.RWMutex.Unlock()
+
+	b.Balance += amount
+}
+
+func (b *BankAccount) GetBalance() int {
+	b.RWMutex.RLock()
+	defer b.RWMutex.RUnlock()
+
+	return b.Balance
+}
+
+func TestReadWriteMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance", account.GetBalance())
+}
+
 func TestMutex(t *testing.T) {
 	x := 0
 	var mutex sync.Mutex
