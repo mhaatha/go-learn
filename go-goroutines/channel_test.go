865b136eeea168316e846d62c970981130479365
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-18 16:43:37 +0800

    Default select

--- go-goroutines/channel_test.go
+++ go-goroutines/channel_test.go
@@ -6,6 +6,34 @@ import (
 	"time"
 )
 
+func TestDefaultSelect(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2:", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data...")
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
+}
+
 func TestSelectChannel(t *testing.T) {
 	channel1 := make(chan string)
 	channel2 := make(chan string)
@@ -25,7 +53,7 @@ func TestSelectChannel(t *testing.T) {
 			fmt.Println("Data dari channel 2:", data)
 			counter++
 		}
-		
+
 		if counter == 2 {
 			break
 		}
