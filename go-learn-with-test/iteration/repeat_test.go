9ae6a09b376aa6bc31b9ff0461659354bf42167e
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-20 20:49:58 +0800

    Test Repeat function with strings Compare and add Testable Example

--- go-learn-with-test/iteration/repeat_test.go
+++ go-learn-with-test/iteration/repeat_test.go
@@ -1,18 +1,28 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
 
-	if repeated != expected {
+	if strings.Compare(repeated, expected) != 0 {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 6)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 6)
+	fmt.Println(repeated)
+	// Output: aaaaaa
 }
