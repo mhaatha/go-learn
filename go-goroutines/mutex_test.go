ad0d3af634e24240ddd499976cc7e23f9c8d7f6b
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-22 21:43:29 +0800

    Deadlock

--- go-goroutines/mutex_test.go
+++ go-goroutines/mutex_test.go
@@ -7,6 +7,60 @@ import (
 	"time"
 )
 
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Lock user1", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(time.Second)
+
+	user2.Lock()
+	fmt.Println("Lock user2", user2.Name)
+	user2.Change(amount)
+
+	time.Sleep(time.Second)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	user1 := UserBalance{
+		Name:    "Eko",
+		Balance: 1000000,
+	}
+
+	user2 := UserBalance{
+		Name:    "Budi",
+		Balance: 1000000,
+	}
+
+	go Transfer(&user1, &user2, 100000)
+	go Transfer(&user2, &user1, 200000)
+
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("User:", user1.Name, "Balance:", user1.Balance)
+	fmt.Println("User:", user2.Name, "Balance:", user2.Balance)
+}
+
 type BankAccount struct {
 	RWMutex sync.RWMutex
 	Balance int
