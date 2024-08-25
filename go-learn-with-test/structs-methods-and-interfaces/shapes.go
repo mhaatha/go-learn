1f629e33ab6d5ba101fd0b3afadab6923e201462
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-25 16:10:42 +0800

    Create test for Perimeter and Area function and make it pass

---
+++ go-learn-with-test/structs-methods-and-interfaces/shapes.go
@@ -0,0 +1,9 @@
+package structs_methods_and_interfaces
+
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width, weight float64) float64 {
+	return width * weight
+}
