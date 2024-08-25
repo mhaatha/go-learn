820ebf6b30739ca9a2b736f7b32fd33099feda36
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-25 16:48:48 +0800

    Refactor the test to make it easy to add a new test and add new Triangle struct that implements Shape

--- go-learn-with-test/structs-methods-and-interfaces/shapes_test.go
+++ go-learn-with-test/structs-methods-and-interfaces/shapes_test.go
@@ -13,21 +13,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10.0}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, at := range areaTests {
+		got := at.shape.Area()
+		if got != at.want {
+			t.Errorf("got %g want %g", got, at.want)
+		}
+	}
 }
