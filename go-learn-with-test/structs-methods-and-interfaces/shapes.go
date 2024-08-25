34eb2e484bd15e810852c5e5936944a53657cfdd
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-25 16:12:37 +0800

    Change weight to height in Area function parameter

--- go-learn-with-test/structs-methods-and-interfaces/shapes.go
+++ go-learn-with-test/structs-methods-and-interfaces/shapes.go
@@ -4,6 +4,6 @@ func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
 
-func Area(width, weight float64) float64 {
-	return width * weight
+func Area(width, height float64) float64 {
+	return width * height
 }
