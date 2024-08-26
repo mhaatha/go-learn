8abb4288387dfd264b2cc7789825f3f1988c766f
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-26 21:47:24 +0800

    Fix the bug on the Withdraw method it will return error if balance less than withdraw value

--- go-learn-with-test/pointer/wallet.go
+++ go-learn-with-test/pointer/wallet.go
@@ -1,6 +1,9 @@
 package pointer
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Stringer interface {
 	String() string
@@ -24,6 +27,11 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
 	w.balance -= amount
+	return nil
 }
--- go-learn-with-test/pointer/wallet_test.go
+++ go-learn-with-test/pointer/wallet_test.go
@@ -5,31 +5,36 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
 
-		want := Bitcoin(10)
-
 		if got != want {
-			t.Errorf("got %s want %s", got, want)
+			t.Errorf("got %s, want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-		got := wallet.Balance()
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
 
-		want := Bitcoin(10)
+		assertBalance(t, wallet, startingBalance)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
 		}
 	})
 }
