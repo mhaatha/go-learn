6e888efcf4d21d0793412628f57a1376b56878af
Hafidz Athaya <mhafidza528@gmail.com>
2024-08-26 21:36:44 +0800

    Refactor the code

--- go-learn-with-test/pointer/wallet.go
+++ go-learn-with-test/pointer/wallet.go
@@ -2,15 +2,24 @@ package pointer
 
 import "fmt"
 
-type Wallet struct {
-	balance int
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
-	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
--- go-learn-with-test/pointer/wallet_test.go
+++ go-learn-with-test/pointer/wallet_test.go
@@ -1,22 +1,19 @@
 package pointer
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
 
-	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
-
-	want := 10
+	want := Bitcoin(20)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
