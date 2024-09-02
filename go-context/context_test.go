2930c4d413e02e999c5c176c3924e6ad9d93c98f
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-02 20:17:37 +0800

    Implement context parent and child using context.WithValue

---
+++ go-context/context_test.go
@@ -0,0 +1,42 @@
+package go_context
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	// Main Parent
+	contextA := context.Background()
+
+	// contextB and contextC are children (child) of contextA
+	contextB := context.WithValue(contextA, "B", "A's Child-1")
+	contextC := context.WithValue(contextA, "C", "A's Child-2")
+
+	contextD := context.WithValue(contextB, "D", "B's Child-1")
+	contextE := context.WithValue(contextB, "E", "B's Child-2")
+
+	contextF := context.WithValue(contextC, "F", "C's Child-1")
+
+	fmt.Println(contextA) // context.Background
+	fmt.Println(contextB) // context.Background.WithValue(type string, val A's Child-1)
+	fmt.Println(contextC) // context.Background.WithValue(type string, val A's Child-2)
+	fmt.Println(contextD) // context.Background.WithValue(type string, val A's Child-1).WithValue(type string, val B's Child-1)
+	fmt.Println(contextE) // context.Background.WithValue(type string, val A's Child-1).WithValue(type string, val B's Child-2)
+	fmt.Println(contextF) // context.Background.WithValue(type string, val A's Child-2).WithValue(type string, val C's Child-1)
+
+	// Get Value
+	fmt.Println(contextC.Value("C")) // Receive value from contextC
+	fmt.Println(contextD.Value("B")) // Receive value from contextD's parent: contextB value
+	fmt.Println(contextE.Value("C")) // nil
+	fmt.Println(contextA.Value("C")) // nil
+}
