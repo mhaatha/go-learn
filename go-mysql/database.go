f01233440d22f0b37efaba0fe9eb11f992a382e2
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-04 15:57:32 +0800

    Implement database pooling in GetConnection function

---
+++ go-mysql/database.go
@@ -0,0 +1,20 @@
+package go_mysql
+
+import (
+	"database/sql"
+	"time"
+)
+
+func GetConnection() *sql.DB {
+	db, err := sql.Open("mysql", "root:dummyPassword_123@tcp(127.0.0.1:3306)/go_database")
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
