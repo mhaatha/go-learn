fd025a09624262c290b4afa2ca4437f01620caac
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-07 07:20:28 +0800

    Embed file to byte

--- go-embed/embed_test.go
+++ go-embed/embed_test.go
@@ -3,6 +3,8 @@ package go_embed
 import (
 	_ "embed"
 	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestEmbed(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed goland.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := os.WriteFile("logo_new.png", logo, 0o644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
