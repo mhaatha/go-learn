a7835769482f026bf9d733d5672c83c1ebad0789
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-07 07:35:36 +0800

    Path Matcher

--- go-embed/embed_test.go
+++ go-embed/embed_test.go
@@ -50,3 +50,17 @@ func TestMultipleFiles(t *testing.T) {
 	}
 	fmt.Println(string(c))
 }
+
+//go:embed files/*.txt
+var path embed.FS
+
+func TestPathMatcher(t *testing.T) {
+	dirEntries, _ := path.ReadDir("files")
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			file, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(file))
+		}
+	}
+}
