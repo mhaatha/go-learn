589505baeb62db0579e42f8ddd15860b835806ba
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-08 19:54:23 +0800

    ServeMux

--- go-web/server_test.go
+++ go-web/server_test.go
@@ -35,3 +35,29 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	var mux *http.ServeMux = http.NewServeMux()
+	mux.HandleFunc("/login", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Masukkan Username:")
+	})
+	mux.HandleFunc("/logout", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Anda berhasil logout")
+	})
+	mux.HandleFunc("/sama/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Sama")
+	})
+	mux.HandleFunc("/sama/dengan", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "=")
+	})
+
+	var server http.Server = http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	var err error = server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
