310cf54c09d645e3c17241c634e705a712670a6b
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-09 09:11:25 +0800

    Multiple query parameter

--- go-web/query_param_test.go
+++ go-web/query_param_test.go
@@ -33,3 +33,27 @@ func TestQueryParameter(t *testing.T) {
 		t.Errorf("Expected '%s' but got '%s'", expectedResponse, bodyString)
 	}
 }
+
+// Multiple Query Parameter
+func FullName(writer http.ResponseWriter, request *http.Request) {
+	firstName := request.URL.Query().Get("first_name")
+	lastName := request.URL.Query().Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestMultipleQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/fullname?first_name=Gustavo&last_name=Fring", nil)
+	recorder := httptest.NewRecorder()
+
+	FullName(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	expectedResponse := "Hello Gustavo Fring"
+	if expectedResponse != bodyString {
+		t.Errorf("Expected '%s' but got '%s'", expectedResponse, bodyString)
+	}
+}
