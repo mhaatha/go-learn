3adfcf23641c7fae12ccc486dde7aa86c047b2a2
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-09 10:03:56 +0800

    Catch response header

--- go-web/header_test.go
+++ go-web/header_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 )
 
+// Request Header
 func RequestHeader(writer http.ResponseWriter, request *http.Request) {
 	contentType := request.Header.Get("content-type")
 	fmt.Fprint(writer, contentType)
@@ -28,5 +29,32 @@ func TestRequestHeader(t *testing.T) {
 	expectedResponse := "application/json"
 	if expectedResponse != bodyString {
 		t.Errorf("Expected '%s' but got '%s'", expectedResponse, bodyString)
+	}
+}
+
+// Response Header
+func ResponseHeader(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("X-Powered-Bye", "mhaatha")
+	fmt.Fprint(writer, "OK")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	expectedResponse := "OK"
+	if expectedResponse != bodyString {
+		t.Errorf("Expected '%s' but got '%s'", expectedResponse, bodyString)
+	}
+
+	headerResponse := response.Header.Get("X-Powered-Bye")
+	if headerResponse != "mhaatha" {
+		t.Errorf("Expected 'mhaatha' but got '%s'", headerResponse)
 	}
 }
