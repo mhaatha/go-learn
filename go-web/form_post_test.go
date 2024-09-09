1dc95d86920fbc624a07cec2aa1401534de34757
Hafidz Athaya <mhafidza528@gmail.com>
2024-09-09 10:34:50 +0800

    Request form

---
+++ go-web/form_post_test.go
@@ -0,0 +1,39 @@
+package go_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Form Post
+func FormPost(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+
+	firstName := request.PostForm.Get("first_name")
+	lastName := request.PostForm.Get("last_name")
+
+	fmt.Fprintf(writer, "Hello %s %s", firstName, lastName)
+}
+
+func TestFormPost(t *testing.T) {
+	requestBody := strings.NewReader("first_name=Gustavo&last_name=Fring")
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/form", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	recorder := httptest.NewRecorder()
+
+	FormPost(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
