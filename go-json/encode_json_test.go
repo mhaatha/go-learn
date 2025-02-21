aa25968aae0e91641403484df8ab540e80145155
Hafidz Athaya <mhafidza528@gmail.com>
2025-02-21 16:42:00 +0800

    Encode Go's data type into JSON using json.Marshal

---
+++ go-json/encode_json_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func logJson(data interface{}) {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(bytes))
+}
+
+func TestEncode(t *testing.T) {
+	logJson("Hafidz")
+	logJson(1)
+	logJson(true)
+	logJson([]string{"Hafidz", "Athaya"})
+}
