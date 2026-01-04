86c82b27de211b054ee19c8b4e7925081d5233b2
Hafidz Athaya <mhafidza528@gmail.com>
2026-01-04 13:37:59 +0800

    dsa: insertion sort

--- go-dsa/sorting/insertion-sort/main.go
+++ go-dsa/sorting/insertion-sort/main.go
@@ -3,30 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	mySlice := []int{2, 8, 5, 3, 9, 4, 1}
+	mySlice := []int{8, 5, 3, 9, 4, 1}
 
 	for i := 1; i < len(mySlice); i++ {
 		key := mySlice[i]
-		lastIndexOfJ := i - 1
+		j := i - 1
 
-		for j := lastIndexOfJ; j >= 0; j-- {
-			if key < mySlice[j] {
-				mySlice[j+1] = mySlice[j]
-			}
-			if key > mySlice[j] {
-				break
-			}
+		for j >= 0 && mySlice[j] > key {
+			mySlice[j+1] = mySlice[j]
+			j--
 		}
 
-		mySlice[lastIndexOfJ+1] = key
+		mySlice[j+1] = key
 	}
 
 	fmt.Println(mySlice)
-
-	// PR:
-	// 1. Kalau key >= mySlice[j]
-	//    maka:
-	//    break
-	//
-	// 2. Swap dilakukan di akhir
 }
