9af6308e8a8b14f1416da896c7e3d546df4a57f2
Hafidz Athaya <mhafidza528@gmail.com>
2026-01-06 19:49:56 +0800

    dsa: merge sort

--- go-dsa/sorting/merge-sort/main.go
+++ go-dsa/sorting/merge-sort/main.go
@@ -1,37 +1,55 @@
 package main
 
 import (
-	"math"
+	"fmt"
 )
 
 func main() {
+	mySlice := []int{8, 5, 3, 9, 4, 1}
 
+	mySortedSlice := mergeSort(mySlice)
+
+	fmt.Println(mySortedSlice)
 }
 
 func mergeSort(items []int) []int {
-	// items = [38, 27, 43, 3]
-
 	// Base case
 	if len(items) < 2 {
 		return items
 	}
 
 	// Divide the slice into two
-	midValue := int(math.Floor(float64(len(items) / 2)))
+	midValue := len(items) / 2
 
-	leftHalf := items[:midValue]  // [38, 27]
-	rightHalf := items[midValue:] // [43, 3]
+	leftHalf := items[:midValue]
+	rightHalf := items[midValue:]
 
-	sortedLeft := mergeSort(leftHalf)
-	sortedRight := mergeSort(rightHalf)
-
-	// Waiting...
+	sortedLeft := mergeSort(leftHalf)   // Pause until finish...
+	sortedRight := mergeSort(rightHalf) // Pause until finish...
 
 	return merge(sortedLeft, sortedRight)
 }
 
 func merge(left, right []int) []int {
 	final := []int{}
+	i := 0
+	j := 0
+
+	for i < len(left) && j < len(right) {
+		// Bandingkan left[i] dengan right[j]
+		// Angka yang lebih kecil, akan diappend ke final setelah itu indexnya++
+		if left[i] < right[j] {
+			final = append(final, left[i])
+			i++
+		} else {
+			final = append(final, right[j])
+			j++
+		}
+	}
+
+	// Cleanup
+	final = append(final, left[i:]...)
+	final = append(final, right[j:]...)
 
 	return final
 }
